68932a423f81fe2221dd854067e959957ad8ed6f
Kenneth Fox <foxk4025@gmail.com>
2025-02-14 07:24:37 -0800

    start with very good core template

---
+++ android/app/src/main/kotlin/com/example/mycoffee/coffee/foo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mycoffee.coffee_foo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:coffee_foo/app/app.dart';
+import 'package:coffee_foo/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
